1883f9d72fd09a104eaf13d61c580e9bd897a81e
lagougou <deraiven@126.com>
2020-08-10 17:57:55 +0800

    add city page

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="header">
+        <router-link to="/">
+        <span class="iconfont back-icon">&#xe624;</span></router-link>
+        
+        城市选择   
+    </div>
+</template>
+<script>
+export default {
+    name: "CityHeader"
+}
+</script>
+<style lang="stylus" scoped>
+    @import "~styles/variable.styl"
+    .header
+        position relative
+        overflow hidden
+        height 0.86rem
+        text-align center
+        color #fff
+        background $bgColor
+        line-height 0.86rem
+        font-size 0.32rem
+        .back-icon
+            width .64rem
+            top 0
+            left 0
+            position absolute
+            text-align center
+            font-size .4rem
+            color #fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,7 +2,9 @@
     <div class="header">
         <div class="header-left"><span class="iconfont back-icon">&#xe624;</span></div>
         <div class="header-input"><span class="iconfont search-icon">&#xe632;</span>输入/城市/景点</div>
-        <div class="header-right">城市<span class="iconfont arrow-down">&#xe64a;</span></div>
+        <router-link to="/city">
+            <div class="header-right">城市<span class="iconfont arrow-down">&#xe64a;</span></div>
+        </router-link>
     </div>
 </template>
 <script>
@@ -42,6 +44,7 @@ export default {
             width 1.24rem
             float right
             text-align center
+            color #fff
             .arrow-down
                 margin-left .05rem
                 font-size .24rem
